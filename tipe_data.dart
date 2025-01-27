cda4d1fc8084e0f2f8b26326af0d2585c5773cb2
faaza mumtaza <faazamksekolah@gmail.com>
2025-01-27 16:32:44 +0700

    Add files via upload

---
+++ tipe_data.dart
@@ -0,0 +1,13 @@
+void main() {
+  var name = "Dart";
+  int age = 20;
+  double height = 1.80;
+  bool isStudent = false;
+  dynamic dynamicType = "Dart";
+
+  print('Name: $name ');
+  print('Age: $age ');
+  print('Height: $height ');
+  print('Is Student: $isStudent ');
+  print('Anything: $dynamicType ');
+}
